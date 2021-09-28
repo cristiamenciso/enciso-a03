8ef0e67f40135ec1bb380b24fc79e960620372a4
Cristiam Enciso <encisocristiam@gmil.com>
2021-09-28 15:15:34 -0400

    Solution26 Skeleton

---
+++ exercise26/src/main/java/baseline/PaymentCalculator.java
@@ -0,0 +1,34 @@
+package baseline;
+
+public class PaymentCalculator {
+    // make private variables
+    private String balance;
+    private String APR;
+    private String monthlyPayment;
+
+    // make get ans set methods
+    public void setBalance(String balance) {
+
+    }
+    public void setAPR(String APR) {
+
+    }
+    public void setMonthlyPayment(String monthlyPayment) {
+
+    }
+
+    public String getBalance() {
+
+    }
+
+    public String getAPR() {
+
+    }
+
+    public String getMonthlyPayment() {
+
+    }
+    public double calculateMonthsUntilPayment(String balance, String APR, String monthlyPayment) {
+
+    }
+}
---
+++ exercise26/src/main/java/baseline/Solution26.java
@@ -0,0 +1,9 @@
+package baseline;
+
+public class Solution26 {
+    public static void main(String[] args) {
+        // get user input
+        // call method to calculate amount of time to pay off credit card
+        // print out results
+    }
+}
