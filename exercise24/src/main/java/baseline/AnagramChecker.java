da2e3080ff6fb186501f909bdf76fb4dc63d2a2a
Cristiam Enciso <encisocristiam@gmil.com>
2021-09-27 22:54:28 -0400

    Solution 24 initial solve

--- exercise24/src/main/java/baseline/AnagramChecker.java
+++ exercise24/src/main/java/baseline/AnagramChecker.java
@@ -1,23 +1,67 @@
 package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Cristiam Enciso
+ */
+
+import java.util.Scanner;
 
 public class AnagramChecker {
     // make variables for anagram checker
     private String word1;
     private String word2;
 
-    // Make a constructor
-    public AnagramChecker(String word1, String word2){
-
+    // Make set methods
+    public void setWord1(String word1) {
+        this.word1 = word1;
     }
+    public void setWord2(String word2) {
+        this.word2 = word2;
+    }
+
     // make get methods
     public String getWord1() {
-
+        return word1;
     }
     public String getWord2() {
-
+        return word2;
     }
     // make method to check if two string are anagrams
-    public boolean isAnagram(String word1, String word2) {
+    public boolean isAnagram() {
+        if (getWord1().length() != getWord2().length()) {
+            return false;
+        }
+        int charRange = 256;
+        int count[] = new int[charRange];
+        for (int i = 0; i < getWord1().length(); i++) {
+            count[getWord1().charAt(i)]++;
+            count[getWord1().charAt(i)]--;
+        }
+        for (int i = 0; i < charRange; i++) {
+            if (count[i] != 0) {
+                return false;
+            }
+        }
+        return true;
 
+    }
+    // Made input and output method in class for ease of access to variables
+
+    public void getInput() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter two strings and I'll tell you if they are anagrams: \nEnter the first string: ");
+        setWord1(in.nextLine());
+        System.out.print("Enter the second string: ");
+        setWord2(in.nextLine());
+    }
+    public String printOutput(boolean result) {
+        String output;
+        if(result) {
+            output = "\"" + getWord1() + "\"" + " amd " + "\"" + getWord2() + "\"" + " are anagrams.";
+        }else {
+            output = "\"" + getWord1() + "\"" + " amd " + "\"" + getWord2() + "\"" + " are not anagrams.";
+        }
+
+        return output;
     }
 }
--- exercise24/src/main/java/baseline/Solution24.java
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -4,17 +4,17 @@ package baseline;
  *  Copyright 2021 Cristiam Enciso
  */
 
+import java.util.Scanner;
 public class Solution24 {
+    static AnagramChecker checker;
     public static void main(String[] args) {
+        AnagramChecker checker = new AnagramChecker();
         // Prompt user for input
-        // Call anagram checker with the input
+        checker.getInput();
+        // Call method to determine if strings are anagrams
         // Display results
+        System.out.println(checker.printOutput(checker.isAnagram()));
 
     }
-    public void getInput() {
 
-    }
-    public void printOutput() {
-
-    }
 }
