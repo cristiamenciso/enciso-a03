16af1cd5f5ac2e12a95e4731cb77fcd16628cbe9
Cristiam Enciso <encisocristiam@gmil.com>
2021-09-27 21:42:23 -0400

    Solution24 Skeleton

---
+++ exercise24/src/main/java/baseline/AnagramChecker.java
@@ -0,0 +1,23 @@
+package baseline;
+
+public class AnagramChecker {
+    // make variables for anagram checker
+    private String word1;
+    private String word2;
+
+    // Make a constructor
+    public AnagramChecker(String word1, String word2){
+
+    }
+    // make get methods
+    public String getWord1() {
+
+    }
+    public String getWord2() {
+
+    }
+    // make method to check if two string are anagrams
+    public boolean isAnagram(String word1, String word2) {
+
+    }
+}
---
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -0,0 +1,20 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Cristiam Enciso
+ */
+
+public class Solution24 {
+    public static void main(String[] args) {
+        // Prompt user for input
+        // Call anagram checker with the input
+        // Display results
+
+    }
+    public void getInput() {
+
+    }
+    public void printOutput() {
+
+    }
+}
