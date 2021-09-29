5781767808d64b178f1918cfc0c1e515c7128f60
Cristiam Enciso <encisocristiam@gmil.com>
2021-09-29 15:01:55 -0400

    Solution 29 Skeleton

---
+++ exercise29/src/main/java/baseline/InputValidator.java
@@ -0,0 +1,29 @@
+package baseline;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Cristiam Enciso
+ */
+
+public class InputValidator {
+    // make variables
+    private String r;
+
+
+    // make constructor and get methods
+    public InputValidator(String r) {
+        this.r = r;
+    }
+
+    public String getR() {
+        return r;
+    }
+    // method to validate input
+    public double validateInput(String r) {
+
+    }
+    // method to calculate years
+    public int calculateYears(double r) {
+
+    }
+}
---
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -0,0 +1,13 @@
+package baseline;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Cristiam Enciso
+ */
+public class Solution29 {
+    public static void main(String[] args) {
+        // get user input
+        // validate user input
+        // print out results
+    }
+}
