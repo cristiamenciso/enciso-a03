82f192d06e5a3421c4c2f4c9f594c471a41cb1d5
Cristiam Enciso <encisocristiam@gmil.com>
2021-09-29 17:31:57 -0400

    Solution 30 Skeleton

---
+++ exercise30/src/main/java/baseline/MultiplicationTable.java
@@ -0,0 +1,12 @@
+package baseline;
+
+public class MultiplicationTable {
+    // method to generate a table
+    public void makeTable(int[][] array) {
+
+    }
+    // method to display table 
+    public void displayTable(int[][] array) {
+
+    }
+}
---
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -0,0 +1,9 @@
+package baseline;
+
+public class Solution30 {
+    public static void main(String[] args) {
+        // initiade 2 dimensional array
+        // call function to print out table
+
+    }
+}
