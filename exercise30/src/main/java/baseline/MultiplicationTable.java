fe1cc1482b094378dfa3d9f570be7fed52b06e53
Cristiam Enciso <encisocristiam@gmil.com>
2021-09-30 12:39:28 -0400

    Solution 30 base

--- exercise30/src/main/java/baseline/MultiplicationTable.java
+++ exercise30/src/main/java/baseline/MultiplicationTable.java
@@ -1,12 +1,32 @@
 package baseline;
 
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Cristiam Enciso
+ */
+
 public class MultiplicationTable {
     // method to generate a table
-    public void makeTable(int[][] array) {
-
+    public int[][] makeTable(int[][] array) {
+        for(int i = 0; i < 12; i++) {
+            for(int j = 0; j < 12; j++) {
+                array[i][j] = (i + 1) * (j + 1);
+            }
+        }
+        return array;
     }
-    // method to display table 
+    // method to display table
     public void displayTable(int[][] array) {
+        for(int i = 0; i < 12; i++) {
+            for(int j = 0; j < 12; j++) {
+                System.out.printf("%4d", array[i][j]);
+            }
+            System.out.println();
+        }
+
+
 
     }
+    
+
 }
--- exercise30/src/main/java/baseline/Solution30.java
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -1,9 +1,19 @@
 package baseline;
 
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Cristiam Enciso
+ */
+
 public class Solution30 {
     public static void main(String[] args) {
+        MultiplicationTable table = new MultiplicationTable();
         // initiade 2 dimensional array
+        int[][] array = new int[12][12];
+        // method to make the multiplication table on the 2d array
+        int[][] multArray =  table.makeTable(array);
         // call function to print out table
+        table.displayTable(multArray);
 
     }
 }
